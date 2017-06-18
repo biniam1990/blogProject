715c29987518b67f9ee317c950b320b3d4132311
biniam1990 <biniamw1990@gmail.com>
2017-06-18 13:50:00 -0500

    minor changes

--- BlogPost-service/src/main/java/org/cs544/project/domain/Role.java
+++ BlogPost-service/src/main/java/org/cs544/project/domain/Role.java
@@ -6,19 +6,11 @@ import javax.persistence.Id;
 
 @Entity
 public class Role {
-<<<<<<< HEAD
+
 	@Id
 	@GeneratedValue
 	private int id;
-=======
- 
-	@Id
-	@GeneratedValue
-	private int id;
-	
- 
->>>>>>> f9a3132ae00df97f6e9d52d0995a69deaf3a0131
-	private User user;
+ User user;
 
 	public User getUser() {
 		return user;
