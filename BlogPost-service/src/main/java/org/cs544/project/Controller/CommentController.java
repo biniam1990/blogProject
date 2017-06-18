1ee91cd255bf26f4d63975b2112f377fd6da0342
AyalewGM <ayalew.mersha@gmail.com>
2017-06-18 16:07:13 -0500

    Version 3

--- BlogPost-service/src/main/java/org/cs544/project/Controller/CommentController.java
+++ BlogPost-service/src/main/java/org/cs544/project/Controller/CommentController.java
@@ -1,5 +1,45 @@
 package org.cs544.project.Controller;
 
-public class CommentController {
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.cs544.project.domain.Comment;
+import org.cs544.project.service.CommentService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/posts")
+public class CommentController {
+	
+	@Resource 
+	private CommentService commentService;
+	
+	
+	@RequestMapping(value="/{postId}/comments", method= RequestMethod.GET)
+	
+	public List<Comment> getComments(@PathVariable int postId){
+		
+			return commentService.getComment(postId);
+				
+	}
+
+@RequestMapping(value="/{postId}/comments/{commentId}", method= RequestMethod.GET)
+	
+	public Comment getOneComment(@PathVariable int postId, @PathVariable int commentId){
+		
+			return getComments(postId).get(commentId);
+				
+	}
+
+@RequestMapping(value="/{postId}/comments", method=RequestMethod.POST)
+public Comment addComment(@RequestBody Comment comment, @PathVariable int postId){
+	
+	return commentService.addComment(comment, postId);
+}
+	
 }
