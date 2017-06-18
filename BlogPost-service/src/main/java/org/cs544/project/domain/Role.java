f9a3132ae00df97f6e9d52d0995a69deaf3a0131
AyalewGM <ayalew.mersha@gmail.com>
2017-06-18 13:39:14 -0500

    Version 2

--- BlogPost-service/src/main/java/org/cs544/project/domain/Role.java
+++ BlogPost-service/src/main/java/org/cs544/project/domain/Role.java
@@ -1,17 +1,17 @@
 package org.cs544.project.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Role {
-<<<<<<< HEAD
-	int id;
-=======
+ 
 	@Id
 	@GeneratedValue
 	private int id;
 	
->>>>>>> 5e163baff5929387303470ea68686ddbcc952d43
+ 
 	private User user;
 
 	public User getUser() {
